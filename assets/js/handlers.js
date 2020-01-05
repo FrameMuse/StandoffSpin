84333438c2484243b082e3c1966d1528b3c03cec
FrameMuse <minicablestone@gmail.com>
2020-01-05 21:48:36 +0400

    v0.3

--- assets/js/handlers.js
+++ assets/js/handlers.js
@@ -3,3 +3,13 @@
 features.sound.onclick = function (tap) {
     console.log("Sound has been turned " + tap);
 }
+
+features.lang.onclick = function (tap) {
+    console.log("Language has been switched to " + tap);
+}
+
+features.timer.timer({
+    seconds: 14,
+    hours: 2,
+    minutes: 6,
+});
--- assets/js/hangouts.js
+++ assets/js/hangouts.js
@@ -1,9 +1,34 @@
 // Extending Functions
 
+String.prototype.split = function() {
+    var output = [],
+        string = this.toString(),
+        length = string.length;
+
+    for (var i = 0; i < length; i++) {
+        output.push(+string.charAt(i));
+    }
+
+    return output;
+}
+
+// Jquery
+
 $.fn.extend({
     toggleText: function (a, b = null) {
         return this.html(this.html() == b ? a : b);
-    }
+    },
+    split: function () {
+        var output = [],
+            string = this.text().toString(),
+            length = string.length;
+
+        for (var i = 0; i < length; i++) {
+            output.push(+string.charAt(i));
+        }
+
+        return output;
+    },
 });
 
 // Events
@@ -53,12 +78,72 @@ class features_sound {
     }
 }
 
+class features_timer {
+    constructor() {
+        this.data = {
+            numbers: {
+                seconds: 0,
+                minutes: 0,
+                hours: 0,
+            },
+        };
+    }
+
+    counts(data = "") {
+        return $(".timer-v2__count[data-count='" + data + "']").find("span");
+    }
+
+    async timer(numbers = {}, speedup = 1) {
+        this.data.numbers = numbers;
+        return await new Promise(resolve => {
+            var interval = setInterval(() => {
+                    // Calculating
+                    if (this.single_discount("seconds", 59))
+                        if (this.single_discount("minutes", 59)) 
+                            if (this.single_discount("hours", 24)) {
+                                clearInterval(interval);
+                                resolve();
+                                return;
+                            }
+                    
+                    // Commiting changes in HTML
+                    for (const property in numbers) {
+                        this.counts(property).each(function (i) {
+                            $(this).html(split(numbers[property], 2)[i]);
+                        });
+                    }
+                }, 1000 / speedup);
+        });
+    }
+
+    single_discount($var = "", Default = 0) {
+        if (this.data["numbers"][$var] == undefined) return true;
+        if (this.data["numbers"][$var] <= 0) {
+            this.data["numbers"][$var] = Default;
+            return true;
+        } else this.data["numbers"][$var]--;
+    }
+}
+
 const features = new class {
     constructor() {
         this.sound = new features_sound();
         this.lang = new features_lang();
+        this.timer = new features_timer();
     }
 }
 
 // Functions
 
+function split(number = 0, bandwidth = 0) {
+    var string = number.toString(),
+        length = string.length,
+        bandwidth = length >= bandwidth ? 0 : bandwidth - length,
+        output = new Array(bandwidth).fill(0);
+
+    for (var i = 0; i < length; i++) {
+        output.push(+string.charAt(i));
+    }
+
+    return output;
+}
