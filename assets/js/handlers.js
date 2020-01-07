afba13335b75ebce7354498dfe26e955abbd49e8
FrameMuse <minicablestone@gmail.com>
2020-01-07 04:32:17 +0400

    v0.6

--- assets/js/handlers.js
+++ assets/js/handlers.js
@@ -1,5 +1,7 @@
 // Handlers
 
+// Minor Functions
+
 features.sound.onclick = function (tap) {
     console.log("Sound has been turned " + tap);
 }
@@ -8,8 +10,28 @@ features.lang.onclick = function (tap) {
     console.log("Language has been switched to " + tap);
 }
 
-features.timer.timer({
+// Timer
+
+features.timer.initiate({
     seconds: 14,
     hours: 2,
-    minutes: 6,
+    minutes: 6, 
 });
+
+console.log("Timer", features.timer.promise);
+
+// Wheel
+
+features.wheel.win({
+    id: 14,
+    skin: {
+        image: "https://standoffcase.ru/img/hFRns34E7g-Screenshot_8.jpg",
+        title: "Skin бомжа",
+    },
+});
+
+console.log("Wheel", features.wheel.promise);
+
+features.wheel.reopen.onclick = function () {
+    console.log("The wheel has been reopenned");
+}
--- assets/js/hangouts.js
+++ assets/js/hangouts.js
@@ -1,6 +1,6 @@
 // Extending Functions
 
-String.prototype.split = function() {
+String.prototype.num_split = function() {
     var output = [],
         string = this.toString(),
         length = string.length;
@@ -18,7 +18,7 @@ $.fn.extend({
     toggleText: function (a, b = null) {
         return this.html(this.html() == b ? a : b);
     },
-    split: function () {
+    num_split: function () {
         var output = [],
             string = this.text().toString(),
             length = string.length;
@@ -93,9 +93,9 @@ class features_timer {
         return $(".timer-v2__count[data-count='" + data + "']").find("span");
     }
 
-    async timer(numbers = {}, speedup = 1) {
+    async initiate(numbers = {}, speedup = 1) {
         this.data.numbers = numbers;
-        return await new Promise(resolve => {
+        this.promise = new Promise(resolve => {
             var interval = setInterval(() => {
                     // Calculating
                     if (this.single_discount("seconds", 59))
@@ -109,11 +109,13 @@ class features_timer {
                     // Commiting changes in HTML
                     for (const property in numbers) {
                         this.counts(property).each(function (i) {
-                            $(this).html(split(numbers[property], 2)[i]);
+                            $(this).html(num_split(numbers[property], 2)[i]);
                         });
                     }
                 }, 1000 / speedup);
         });
+        
+        return await this.promise;
     }
 
     single_discount($var = "", Default = 0) {
@@ -125,17 +127,112 @@ class features_timer {
     }
 }
 
+class features_wheel {
+    constructor() {
+        var points = {};
+        $(".fortune__circle > *").each(function (i, e) {
+            points[i] = $(e);
+        });
+        this.reopen = {
+            onclick: function () { },
+        };
+        this.data = {
+            points,
+            spins: 4, // Кол-во оборотов перед тем как дойдёт до нужного блока
+            animation: $(".fortune__circle").css("transition"),
+            duration: 14,
+        };
+        this.count(18);
+
+        $(".js-wheel-reopen").click(() => {
+            $(".fortune__circle, .fortune-wheel__inner, .fortune-wheel__curve-1, .fortune-wheel__curve-2").toggleClass("hidden");
+            $(".fortune-wheel").removeClass("fortune-wheel--without-after");
+            this.reopen.onclick();
+        });
+    }
+
+    getRotationDegrees(obj) {
+        var matrix = obj.css("-webkit-transform") ||
+            obj.css("-moz-transform") ||
+            obj.css("-ms-transform") ||
+            obj.css("-o-transform") ||
+            obj.css("transform");
+        if (matrix !== 'none') {
+            var values = matrix.split('(')[1].split(')')[0].split(',');
+            var a = values[0];
+            var b = values[1];
+            var angle = Math.round(Math.atan2(b, a) * (180 / Math.PI));
+            
+        } else { var angle = 0; }
+        return (angle < 0) ? angle + 360 : angle;
+    }
+
+    spinTo(id) {
+        var block = this.data.points[id];
+        var degrees = (this.data.spins * 360) - this.getRotationDegrees(block);
+        var rotate = "rotate(" + degrees + "deg)";
+        $(".fortune__circle").css({ transform: "", transition: "unset", animation: "unset" })
+        setTimeout(() => {
+            $(".fortune__circle").css({ "transform": rotate, transition: this.data.animation });
+        }, 200);
+        return block;
+    }
+
+    checkUp(obj) {
+        return this.promise = new Promise(resolve => {
+            setTimeout(() => {
+                var interval = setInterval(() => {
+                    console.log(this.getRotationDegrees(obj));
+                    console.log(this.getRotationDegrees($(".fortune__circle")));
+                    
+                    if ((this.getRotationDegrees(obj) + this.getRotationDegrees($(".fortune__circle"))) == 360) {
+                        clearInterval(interval);
+                        resolve();
+                        return;
+                    }
+                }, 500);
+            }, this.data.duration * 1000);
+        });
+    }
+
+    async win(data = {}) {
+        var block = this.spinTo(data.id);
+        setTimeout(() => {
+            block.find(".weapon-skins__image").attr({ src: data.skin.image });
+            block.find(".sorted-skins__skin-title").html(data.skin.title);
+        }, 3000);
+        await this.checkUp(block);
+        $(".fortune__circle, .fortune-wheel__inner, .fortune-wheel__curve-1, .fortune-wheel__curve-2").toggleClass("hidden");
+        $(".fortune-wheel").addClass("fortune-wheel--without-after");
+        return this.promise;
+    }
+
+    count(n) {
+        var height = +$(".fortune__item").css("height").replace("px", "");
+
+        console.log("You have " + n + " block in it");
+        console.log("Each block has " + (360 / n) + " degrees");
+        console.log("Each block has " + (114 / (360 / n)) + " ratio");
+        console.log("Each block width is " + (height / (114 / (360 / n))) + "px");
+
+        $(".fortune__item").each(function (i) {
+            $(this).css("transform", "rotate(" + ((360 / n) * i) + "deg) translateY(var(--wheel-indent))");
+        });
+    }
+}
+
 const features = new class {
     constructor() {
         this.sound = new features_sound();
         this.lang = new features_lang();
         this.timer = new features_timer();
+        this.wheel = new features_wheel();
     }
 }
 
 // Functions
 
-function split(number = 0, bandwidth = 0) {
+function num_split(number = 0, bandwidth = 0) {
     var string = number.toString(),
         length = string.length,
         bandwidth = length >= bandwidth ? 0 : bandwidth - length,
