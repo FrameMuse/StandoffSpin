d1f21b42132228dc80bb36ec6d2d875dd8809db7
FrameMuse <minicablestone@gmail.com>
2020-02-25 21:25:35 +0400

    v1.35

--- assets/js/handlers.js
+++ assets/js/handlers.js
@@ -298,69 +298,54 @@ $(document).on("click", ".contract-window__button", function () {
 
 // Popup
 
-$(document).on("click", ".popup-window__close, .popup__cover", () => {
-    page.popup.fadeOut();
-});
+page.popup.on["withdraw"] = function (options) {
+    var itemPrice = convert(parseFloat(options.item.price) + parseFloat(options.rnd));
+    var item = options.item;
+    this.wEdit({
+        summary: this.summary.replace('{itemPrice}', itemPrice),
+        content: `<div style="display:flex;justify-content:center;flex-direction:column;align-items:center;"><input class="popup-window__button button1" withdraw-skin placeholder="${this.wText("other.placeholder")}"><button class="popup-window__button button2" onclick="withdrawConfirm(${item.id}, ${options.rnd})">${this.wText("other.button")}</button></div>`,
+    });
+};
 
-page.popup.on = function ($window, options = {}) {
-    switch ($window) {
-        case "withdraw":
-            let itemPrice = convert(parseFloat(options.item.price) + parseFloat(options.rnd));
-            let item = options.item;
-            this.wEdit({
-                title: this.title,
-                summary: this.summary.replace('{itemPrice}', itemPrice),
-                content: `<div style="display:flex;justify-content:center;flex-direction:column;align-items:center;"><input class="popup-window__button button1" withdraw-skin placeholder="${getLanguage(`popup.${$window}.other.placeholder`)}"><button class="popup-window__button button2" onclick="withdrawConfirm(${item.id}, ${options.rnd})">${getLanguage(`popup.${$window}.other.button`)}</button></div>`,
-            });
-            break;
+page.popup.on["withdraw_error"] = function () {
+    // Automataticly Filled
+};
 
-        case "withdraw_error":
-            this.wEdit({
-                title: this.title,
-                summary: this.summary,
-            });
-            break;
+page.popup.on["withdraw_avatar"] = function () {
+    this.wEdit({
+        content: "<form class='avatar-change' method='POST' enctype='multipart/form-data' action='/api/v1/user/avatar'><div class='avatar-change__title'>" + getLanguage("popup.withdraw_avatar.avatar_change__title") + "</div><img src='/img/guest.png' class='avatar-change__avatar'><label><a class='avatar-change__input button1'>" + getLanguage("popup.withdraw_avatar.avatar_change__input") + "</a><input accept='.jpg, .png, .jpeg, .gif, .bmp, .tif, .tiff|image/*' type='file' onchange='this.form.submit();' name='file' style='display: none; position: absolute;' ></label></form>",
+    });
+};
 
-        case "withdraw_avatar":
-            this.wEdit({
-                title: this.title,
-                summary: this.summary,
-                content: "<form class='avatar-change' method='POST' enctype='multipart/form-data' action='/api/v1/user/avatar'><div class='avatar-change__title'>" + getLanguage("popup.withdraw_avatar.avatar_change__title") + "</div><img src='/img/guest.png' class='avatar-change__avatar'><label><a class='avatar-change__input button1'>" + getLanguage("popup.withdraw_avatar.avatar_change__input") + "</a><input accept='.jpg, .png, .jpeg, .gif, .bmp, .tif, .tiff|image/*' type='file' onchange='this.form.submit();' name='file' style='display: none; position: absolute;' ></label></form>",
-            });
-            break;
+page.popup.on["auth"] = function () {
+    this.wEdit({
+        content: '<div class="auth"><a href="/auth/vk" class="auth__button auth__button--vkontakte">Вконтакте</a><a href="/auth/twitch" class="auth__button auth__button--twitch">Twitch</a><a href="/auth/youtube" class="auth__button auth__button--youtube">Youtube</a></div>',
+        help: this.wText("help"),
+    });
+};
 
-        case "auth":
-            this.wEdit({
-                title: this.title,
-                content: '<div class="auth"><a href="/auth/vk" class="auth__button auth__button--vkontakte">Вконтакте</a><a href="/auth/twitch" class="auth__button auth__button--twitch">Twitch</a><a href="/auth/youtube" class="auth__button auth__button--youtube">Youtube</a></div>',
-                help: getLanguage(`popup.${$window}.help`),
-            });
-            break;
-        case "sell_all":
-            this.wEdit({
-                title: this.title,
-                summary: this.summary.replace("{price}", options.inventoryPrice),
-                content: `<div style="display:flex;justify-content:center;"><button class="popup-window__button button2" onclick="page.popup.close();">${getLanguage("popup." + $window + ".buttons.no")}</button><button class="popup-window__button button1" onclick="ItemsController.sellAll();">${getLanguage("popup." + $window + ".buttons.yes")}</button></div>`,
-            });
-            break;
-        case "sell_all_error":
-            this.wEdit({
-                title: this.title,
-                summary: this.summary,
-            });
-            break;
-        case "0ge":
-            this.wEdit({
-                title: this.title,
-                summary: this.summary.replace("{skin}", options.skin),
-            });
-            break;
+page.popup.on["sell_all"] = function (options) {
+    this.wEdit({
+        summary: this.summary.replace("{price}", options.inventoryPrice),
+        content: `<div style="display:flex;justify-content:center;"><button class="popup-window__button button2" onclick="page.popup.close();">${this.wText("buttons.no")}</button><button class="popup-window__button button1" onclick="ItemsController.sellAll();">${this.wText("buttons.yes")}</button></div>`,
+    });
+};
 
-        case "top_up":
-            this.wEdit({
-                title: "Пополнение баланса",
-                summary: "Для пополнения баланса вы будете перемещены на сайт платёжной системы",
-                content: `<div class="top-up">
+page.popup.on["sell_all_error"] = function () {
+    // Automataticly Filled
+};
+
+page.popup.on["0ge"] = function (options) {
+    this.wEdit({
+        summary: this.summary.replace("{skin}", options.skin),
+    });
+};
+
+page.popup.on["top_up"] = function (options) {
+    this.wEdit({
+        // title: "Пополнение баланса",
+        // summary: "Для пополнения баланса вы будете перемещены на сайт платёжной системы",
+        content: `<div class="top-up">
                             <div class="promocode">
                                 <input type="text" class="promocode__input" placeholder="Промокод">
                                 <span class="promocode__text">Если у вас есть промокод введите выше</span>
@@ -370,48 +355,40 @@ page.popup.on = function ($window, options = {}) {
                                 <input type="submit" class="top-up__button" value="Пополнить">
                             </div>
                         </div>`,
-                help: '<div class="paysys paysys--1"><img src="https://standoffcase.ru/assets/img/payments_systems/mastercard.svg" class="paysys__image"><img src="https://standoffcase.ru/assets/img/payments_systems/webmoney.svg" class="paysys__image"><img src="https://standoffcase.ru/assets/img/payments_systems/qiwi.svg" class="paysys__image"><img src="https://standoffcase.ru/assets/img/payments_systems/yandex.svg" class="paysys__image"><img src="https://standoffcase.ru/assets/img/payments_systems/visa.svg" class="paysys__image"></div>',
-            });
-            $(".popup-window__top-up--big-input").on('input', function () {
-                var value = $(this).val();
-                if (value == "") {
-                    $(".top_up_text").text(getLanguage("popup.top_up.promocode.default"));
-                    return;
-                }
-                $.get("/api/v1/user/promo?name=" + encodeURIComponent(value), (data) => {
-                    if (data.percent) {
-                        $(".top_up_text").text(getLanguage("popup.top_up.promocode.percent").replace("{percent}", data.percent));
-                    } else {
-                        $(".top_up_text").text(getLanguage("popup.top_up.promocode.not_found"));
-                    }
-                });
-            });
-            break;
-
-        case "vk-accept":
-            this.wEdit({
-                content: '<div class="vk-accept"> <section> <div class="vk-accept__title">' + this.wText("sections.1") + '</div> <div class="vk-accept__content"> <div class="mauto" id="vk_groups"></div> </div> </section> <section> <div class="vk-accept__title">' + this.wText("sections.2") + '</div> <div class="vk-accept__content"> <div class="mauto width150" id="vk_allow_messages_from_community"></div> </div> </section> <section> <div class="vk-accept__title">' + this.wText("sections.3") + '</div></section> <button onclick="_closePopup()" class="button1">' + this.wText("button") + '</button> </div>',
-            });
-
-            // VK Widgets
-            VK.Widgets.AllowMessagesFromCommunity("vk_allow_messages_from_community", { height: 30 }, 187346506);
-            VK.Widgets.Group("vk_groups", { mode: 1, no_cover: 1 }, 187346506);
-            break;
-
-        case "pay_confirm":
-            this.wEdit({
-                title: this.title,
-                summary: this.summary,
-                content: `<div style="display:flex;justify-content:center;"><button class="popup-window__button button2" onclick="${options.callback1}">${getLanguage("popup." + $window + ".buttons.yes")}</button><button class="popup-window__button button1" onclick="${options.callback2}">${getLanguage("popup." + $window + ".buttons.no")}</button></div>`,
-            });
-            break;
-
-        // Default
-        default:
-            this.wEdit({
-                title: this.title,
-                summary: this.summary,
-            });
-            break;
-    }
+        help: '<div class="paysys paysys--1"><img src="https://standoffcase.ru/assets/img/payments_systems/mastercard.svg" class="paysys__image"><img src="https://standoffcase.ru/assets/img/payments_systems/webmoney.svg" class="paysys__image"><img src="https://standoffcase.ru/assets/img/payments_systems/qiwi.svg" class="paysys__image"><img src="https://standoffcase.ru/assets/img/payments_systems/yandex.svg" class="paysys__image"><img src="https://standoffcase.ru/assets/img/payments_systems/visa.svg" class="paysys__image"></div>',
+    });
+    $(".popup-window__top-up--big-input").on('input', function () {
+        var value = $(this).val();
+        if (value == "") {
+            $(".top_up_text").text(this.wText("promocode.default"));
+            return;
+        }
+        api.get("/user/promo", { name: value }, function () {
+            if (data.percent) {
+                $(".top_up_text").text(this.wText("promocode.percent").replace("{percent}", data.percent));
+            } else {
+                $(".top_up_text").text(this.wText("promocode.not_found"));
+            }
+        });
+    });
 };
+
+page.popup.on["vk-accept"] = function (options) {
+    this.wEdit({
+        content: '<div class="vk-accept"> <section> <div class="vk-accept__title">' + this.wText("sections.1") + '</div> <div class="vk-accept__content"> <div class="mauto" id="vk_groups"></div> </div> </section> <section> <div class="vk-accept__title">' + this.wText("sections.2") + '</div> <div class="vk-accept__content"> <div class="mauto width150" id="vk_allow_messages_from_community"></div> </div> </section> <section> <div class="vk-accept__title">' + this.wText("sections.3") + '</div></section> <button onclick="_closePopup()" class="button1">' + this.wText("button") + '</button> </div>',
+    });
+
+    // VK Widgets
+    VK.Widgets.AllowMessagesFromCommunity("vk_allow_messages_from_community", { height: 30 }, 187346506);
+    VK.Widgets.Group("vk_groups", { mode: 1, no_cover: 1 }, 187346506);
+};
+
+page.popup.on["pay_confirm"] = function (options) {
+    this.wEdit({
+        content: `<div style="display:flex;justify-content:center;"><button class="popup-window__button button2" onclick="${options.callback1}">${this.wText("buttons.yes")}</button><button class="popup-window__button button1" onclick="${options.callback2}">${this.wText("buttons.no")}</button></div>`,
+    });
+};
+
+$(document).on("click", ".popup-window__close, .popup__cover", () => {
+    page.popup.fadeOut();
+});
